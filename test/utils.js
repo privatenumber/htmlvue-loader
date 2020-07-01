e9c1d5e06418c18aca318ca6bed4e2da77d80a30
Hiroki Osame <hiroki.osame@gmail.com>
2020-07-01 18:51:44 -0400

    fix: treating markup as HTML. Use xmlMode instead

---
+++ test/utils.js
@@ -0,0 +1,85 @@
+const htmlvueLoader = require.resolve('..');
+
+const outdent = require('outdent');
+const webpack = require('webpack');
+const MemoryFS = require('memory-fs');
+const fs = require('fs');
+const { ufs } = require('unionfs');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const Vue = require('vue');
+
+function build(input, options = {}) {
+	return new Promise((resolve, reject) => {
+		const mfs = new MemoryFS();
+
+		if (typeof input === 'function') {
+			input(mfs);			
+		} else {
+			mfs.writeFileSync('/entry.html', input);
+		}
+
+		const compiler = webpack({
+			mode: 'development',
+			resolveLoader: {
+				alias: {
+					'htmlvue-loader': htmlvueLoader
+				},
+			},
+			module: {
+				rules: [
+					{
+						test: /\.vue$/,
+						use: 'vue-loader',
+					},
+					{
+						test: /\.html$/,
+						use: [
+							'vue-loader',
+							{
+								loader: 'htmlvue-loader',
+								options,
+							},
+						],
+					},
+				],
+			},
+			plugins: [
+				new VueLoaderPlugin(),
+			],
+			entry: '/entry.html',
+			output: {
+				path: '/',
+				filename: 'test.build.js',
+			},
+		});
+
+		compiler.inputFileSystem = ufs.use(fs).use(mfs);
+		compiler.outputFileSystem = mfs;
+
+		compiler.run(function (err, stats) {
+			if (err) {
+				reject(err);
+				return;
+			}
+
+			if (stats.compilation.errors.length > 0) {
+				reject(stats.compilation.errors);
+				return;
+			}
+
+			resolve(mfs.readFileSync('/test.build.js').toString());
+		});
+	});
+}
+
+function run(src) {
+	const { default: Component } = eval(src);
+	const vm = new Vue(Component);
+	vm.$mount();
+	return vm;
+}
+
+module.exports = {
+	build,
+	run,
+};
